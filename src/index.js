622bbfd870cb4874fcbb05937f6c92994d2446b7
Vaibhav <vjain0998@gmail.com>
2023-02-26 16:54:24 +0530

    Added get user by id in repository layer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
 
 const app = express();
 
 const prepareAndStartServer = () => {
 
-    app.listen(PORT, () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api', apiRoutes);
+    app.listen(PORT, async () => {
         console.log(`Server started on Port: ${PORT}`);
+
     })
 }
 prepareAndStartServer();
---
+++ src/repository/user-repository.js
@@ -0,0 +1,41 @@
+const { User } = require('../models/index');
+
+class UserRepository {
+    async create(data) {
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw (error);
+        }
+
+    }
+
+    async destroy(userId) {
+        try {
+            await User.destroy({
+                where: {
+                    id: userId
+                }
+            })
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw (error);
+        }
+    }
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
+}
+
+module.exports = UserRepository;
